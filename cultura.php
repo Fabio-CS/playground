a6a7143859daf59e1350056f8d10744f4d5f433a
Saturno <Saturno@Saturno-PC>
2013-06-16 20:11:45 -0300

    Adicionando links na Cultura e Home

--- cultura.php
+++ cultura.php
@@ -5,10 +5,10 @@
         <?php include 'head.php'; ?>
         <script>
             $(document).ready(function(e) {
-                $(".cultura_item").mouseenter(function(){
+                $(".cultura_item").mouseenter(function() {
                     $(this).addClass("ativo");
                 });
-                $(".cultura_item").mouseleave(function(){
+                $(".cultura_item").mouseleave(function() {
                     $(this).removeClass("ativo");
                 });
                 menu("menu_cultura");
@@ -24,12 +24,14 @@
                     <div id="div_esquerda">
                         <h1>Cultura</h1>
                         <div class="cultura_item">
-                            <div class="imagem_item_cultura">
-                               <!-- Colocar Imagem aqui-->
-                            </div>
+                            <a href="cultura-detalhe.php">
+                                <div class="imagem_item_cultura">
+                                    <!-- Colocar Imagem aqui-->
+                                </div>
+                            </a>
                             <div class="info_item_cultura">
-                                <h2>03 de Maio</h2>
-                                <h4>Holiday on ice</h4>
+                                <a href="cultura-detalhe.php"><h2>03 de Maio</h2></a>
+                                <a href="cultura-detalhe.php"><h4>Holiday on ice</h4></a>
                                 <p>
                                     Patinação no gelo com toda a turma da Disney!<br>
                                     Classificação: LIVRE<br>
@@ -40,7 +42,7 @@
                         </div>
                         <div class="cultura_item">
                             <div class="imagem_item_cultura">
-                               <!-- Colocar Imagem aqui-->
+                                <!-- Colocar Imagem aqui-->
                             </div>
                             <div class="info_item_cultura">
                                 <h2>05 de Maio</h2>
@@ -55,7 +57,7 @@
                         </div>
                         <div class="cultura_item">
                             <div class="imagem_item_cultura">
-                               <!-- Colocar Imagem aqui-->
+                                <!-- Colocar Imagem aqui-->
                             </div>
                             <div class="info_item_cultura">
                                 <h2>07 de Maio</h2>
@@ -70,7 +72,7 @@
                         </div>
                         <div class="cultura_item">
                             <div class="imagem_item_cultura">
-                               <!-- Colocar Imagem aqui-->
+                                <!-- Colocar Imagem aqui-->
                             </div>
                             <div class="info_item_cultura">
                                 <h2>12 de Maio</h2>
@@ -85,7 +87,7 @@
                         </div>
                         <div class="cultura_item">
                             <div class="imagem_item_cultura">
-                               <!-- Colocar Imagem aqui-->
+                                <!-- Colocar Imagem aqui-->
                             </div>
                             <div class="info_item_cultura">
                                 <h2>20 de Junho</h2>
@@ -102,8 +104,8 @@
                     <div id="div_direita">
                         <div id="div_boxzoom">
                             <div id="div_fotozoom">
-                                <img src="imagens/img_zoom.png"><br>
-                                <img id="img_tit_zoom" src="imagens/img_zoom_tit.png">
+                                <a href="#"><img src="imagens/img_zoom.png"></a><br>
+                                <a href="#"><img id="img_tit_zoom" src="imagens/img_zoom_tit.png"></a>
                             </div>
                             <p>Festa de Aniversário do Theo</p>
                             <p><span>Grêmio Náutico União</span></p>
