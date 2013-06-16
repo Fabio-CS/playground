d5e7f79d229a03e95435c2395c392285c5e3a2df
Saturno <Saturno@Saturno-PC>
2013-06-16 14:51:24 -0300

    começando a fazer a home

--- index.php
+++ index.php
@@ -16,34 +16,12 @@
                 <?php include 'topo.php'; ?>
                 <div id="div_conteudo">
                     <div id="div_esquerda">
-                        <h1>03 de Maio > Disney on ice</h1>
-                        <p class="legendaAnuncio">Patrocinado</p>
-                        <div class="anuncio660x86 anuncio">
-                            <p><span>660 x 86 px</span></p>
+                        <h1>Cultura</h1>
+                        
+                        <div class="box_cultura destaq">
+                            <div class="foto"></div>
                         </div>
-                        <div id="div_fotoConteudo">
-                            <img src="imagens/img_disney.png" alt="Disney on ice">
-                            <p class="legendaFoto">Por Fernando Lemos | Foto Fotolia</p>    
-                        </div>
-                        <div id="div_textosConteudo">
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.</p>
-                            <p>Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
-                        </div>
-                        <div id="div_agenda">
-                        <h5>Anote na Agenda</h5>
-                            <p>
-                                Data: 24 de junho<br>
-                                Local: Escola São Paulo<br>
-                                Endereço: Rua Augusta, 2239 – São Paulo<br>
-                                Duração: 4 horas<br>
-                                Valor: 280 reais em duas vezes<br>
-                                Mais informações: (11) 3060-3636<br>
-                            </p>
-                        </div>
-                        <div id="div_btn_redes">
-                            <img src="imagens/btn_redes.png">
-                        </div>
+                        
                     </div>
                     <div id="div_direita">
                         <div id="div_boxzoom">
@@ -57,11 +35,8 @@
                         <div class="anuncio250x250 anuncio">
                             <p><span>PUB <br> 250 x 250px</span></p>
                         </div>
-                        <div class="anuncio250x125 anuncio">
-                            <p><span>PUB <br> 250 x 125px</span></p>
-                        </div>
-                        <div class="anuncio250x125 anuncio">
-                            <p><span>PUB <br> 250 x 125px</span></p>
+                        <div class="anuncio250x250 anuncio">
+                            <p><span>PUB <br> 250 x 250</span></p>
                         </div>
                     </div>
                 </div>
