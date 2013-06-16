5517279bdcafa6023da2bf16a3abdb82bf7f11af
Saturno <Saturno@Saturno-PC>
2013-06-16 19:23:58 -0300

    Terminada a pagina ZOOM

--- zoom.php
+++ zoom.php
@@ -14,38 +14,124 @@
             <div id="main_zoom">
                 <?php include 'topo.php'; ?>
                 <div id="conteudo">
-                    
+
                     <div id="div_esquerda">
                         <h1>Zoom</h1>
-                        
+
                         <p class="legendaAnuncio">Patrocinado</p>
                         <div class="anuncio660x86 anuncio">
                             <p><span>660 x 86 px</span></p>
                         </div>
-                        <h2>Álbums</h2>
+
+                        <div id="colecao_albums">
+                            <h2>Álbums</h2>
+
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                            <div class="album">
+                                <div class="img_album"></div>
+                                <p class="alb_tit">Nome do Evento</p>
+                                <p>Local do Evento</p>
+                            </div>
+                        </div>
+
                     </div>
                     <div id="div_direita">
                         <div id="box_pesq">
                             <form action="" method="">
-                            <input type="text" id="pesquisa" name="pesquisa"/>
-                            <input type="submit" id="buscar" name="buscar" value="BUSCAR"/>
+                                <input type="text" id="pesquisa" name="pesquisa"/>
+                                <input type="submit" id="buscar" name="buscar" value="BUSCAR"/>
                             </form>
                         </div>
                         <div id="div_boxhist">
+                            <img class="tit_album" src="imagens/img_tit_album.png" alt="">
                             <ul id="historico">
-                                    <li id="ano"> <h5>2013</h5>
-                                        <ul> 
-                                            <li id="mes"><H6>JANEIRO</H6>
-                                                <ul id="albuns">
+                                <li id="ano"> <h5>2013</h5>
+                                    <ul> 
+                                        <li id="mes"><H6>JANEIRO</H6>
+                                            <ul id="albuns">
                                                 <li>LANÇAMENTO MARCA</li>
                                                 <li>FESTA DE ANIVERSARIO DO THEO</li>
                                                 <li>LANÇAMENTO MARCA</li>
                                                 <li>LANÇAMENTO MARCA</li>
                                                 <li>LANÇAMENTO MARCA</li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
                             </ul>
                         </div>
                         <div class="anuncio250x250 anuncio">
@@ -56,10 +142,10 @@
                         </div>
                     </div>
                 </div>
-                    
-                </div>
-                <?php include 'rodape.php'; ?>
+
             </div>
+            <?php include 'rodape.php'; ?>
         </div>
-    </body>
+    </div>
+</body>
 </html>
