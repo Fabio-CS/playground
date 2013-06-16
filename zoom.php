8b71c14a330a8e997da10516c2ac785dc46b6590
Saturno <Saturno@Saturno-PC>
2013-06-16 18:39:17 -0300

    pagina zoom

--- zoom.php
+++ zoom.php
@@ -14,6 +14,49 @@
             <div id="main_zoom">
                 <?php include 'topo.php'; ?>
                 <div id="conteudo">
+                    
+                    <div id="div_esquerda">
+                        <h1>Zoom</h1>
+                        
+                        <p class="legendaAnuncio">Patrocinado</p>
+                        <div class="anuncio660x86 anuncio">
+                            <p><span>660 x 86 px</span></p>
+                        </div>
+                        <h2>Álbums</h2>
+                    </div>
+                    <div id="div_direita">
+                        <div id="box_pesq">
+                            <form action="" method="">
+                            <input type="text" id="pesquisa" name="pesquisa"/>
+                            <input type="submit" id="buscar" name="buscar" value="BUSCAR"/>
+                            </form>
+                        </div>
+                        <div id="div_boxhist">
+                            <ul id="historico">
+                                    <li id="ano"> <h5>2013</h5>
+                                        <ul> 
+                                            <li id="mes"><H6>JANEIRO</H6>
+                                                <ul id="albuns">
+                                                <li>LANÇAMENTO MARCA</li>
+                                                <li>FESTA DE ANIVERSARIO DO THEO</li>
+                                                <li>LANÇAMENTO MARCA</li>
+                                                <li>LANÇAMENTO MARCA</li>
+                                                <li>LANÇAMENTO MARCA</li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                            </ul>
+                        </div>
+                        <div class="anuncio250x250 anuncio">
+                            <p><span>PUB <br> 250 x 250px</span></p>
+                        </div>
+                        <div class="anuncio250x250 anuncio">
+                            <p><span>PUB <br> 250 x 250</span></p>
+                        </div>
+                    </div>
+                </div>
+                    
                 </div>
                 <?php include 'rodape.php'; ?>
             </div>
